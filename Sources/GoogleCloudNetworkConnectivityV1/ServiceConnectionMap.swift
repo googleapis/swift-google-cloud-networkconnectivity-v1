e71a7bd8309c441f1eead5fcbbbf18943b5d5396
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:01:37 -0400

    chore: update librarian with suppressed diagnostics (#1014)
    In this version of librarian all the 'DeprecatedDeclaration' warnings
    are supressed.
    
    We want these diagnostics because they inform customers when they are
    trying to use a service, method, message, field, enum or enum value that
    has been deprecated.
    
    We also need to use the same elements, if nothing else, to return them
    when the service includes them in a response, or allow customers to keep
    using them until they have time to fix their code.
    
    Until Swift 6.4 we could not supress the diagnostics, which was too
    noisy. With this PR the diagnostics are suppressed, and the CI system
    now can enable warnings for other locations (say examples) in case we
    use the deprecated elements where we shouldn't.
    
    On Swift 6.2 and 6.3, the diagnostics are not suppressed. Not ideal. On
    the other hand, we expect those to go away in 18 months or so.

--- Sources/GoogleCloudNetworkConnectivityV1/ServiceConnectionMap.swift
+++ Sources/GoogleCloudNetworkConnectivityV1/ServiceConnectionMap.swift
@@ -361,6 +361,9 @@ public struct ServiceConnectionMap: Codable, Equatable, GoogleWKT._AnyPackable,
       ]
     }
 
+    #if hasAttribute(diagnose)
+      @diagnose(DeprecatedDeclaration, as: ignored)
+    #endif
     public init(from decoder: Decoder) throws {
       let container = try decoder.container(keyedBy: CodingKeys.self)
       if let value = try container.decodeIfPresent(Swift.String.self, forKey: .project) {
@@ -402,6 +405,9 @@ public struct ServiceConnectionMap: Codable, Equatable, GoogleWKT._AnyPackable,
       }
     }
 
+    #if hasAttribute(diagnose)
+      @diagnose(DeprecatedDeclaration, as: ignored)
+    #endif
     public func encode(to encoder: Encoder) throws {
       var container = encoder.container(keyedBy: CodingKeys.self)
       try container.encode(self.project, forKey: .project)
@@ -684,6 +690,9 @@ public struct ServiceConnectionMap: Codable, Equatable, GoogleWKT._AnyPackable,
       ]
     }
 
+    #if hasAttribute(diagnose)
+      @diagnose(DeprecatedDeclaration, as: ignored)
+    #endif
     public init(from decoder: Decoder) throws {
       let container = try decoder.container(keyedBy: CodingKeys.self)
       if let value = try container.decodeIfPresent(Swift.String.self, forKey: .serviceAttachmentUri)
@@ -736,6 +745,9 @@ public struct ServiceConnectionMap: Codable, Equatable, GoogleWKT._AnyPackable,
       }
     }
 
+    #if hasAttribute(diagnose)
+      @diagnose(DeprecatedDeclaration, as: ignored)
+    #endif
     public func encode(to encoder: Encoder) throws {
       var container = encoder.container(keyedBy: CodingKeys.self)
       try container.encode(self.serviceAttachmentUri, forKey: .serviceAttachmentUri)
